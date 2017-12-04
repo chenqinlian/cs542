e1a55f637b49b7d808b7ca31eb2dbc8c242cd75d
chenqinlian <chenqinlian27@gmail.com>
2017-12-04 02:03:01 -0500

    Test

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
         StartFrame frame = new StartFrame();
 		
